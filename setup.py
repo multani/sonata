5052527bd38fd1a5e4c119ed4f0669dfd1a7638f
Scott Horowitz <stonecrest@gmail.com>
2008-01-12 21:39:52 +0000

    bump to 1.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ for lang in ('de', 'pl', 'ru', 'fr', 'zh_CN', 'sv', 'es', 'fi', 'uk', 'it', 'cs'
     os.system("msgfmt %s -o %s" % (pofile, mofile))
 
 setup(name='Sonata',
-        version='1.3',
+        version='1.4',
         description='GTK+ client for the Music Player Daemon (MPD).',
         author='Scott Horowitz',
         author_email='stonecrest@gmail.com',
