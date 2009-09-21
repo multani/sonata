tuukka/sonata-svn-test

Preferences: behavior toggle buttons apply instantly

sonata/main.py
@@ -2873,7 +2873,7 @@ class Base(object):
 		trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and 
 				    self.statusicon.get_visible())
 				   or (HAVE_EGG and self.trayicon.get_property('visible')))
-		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed)
+		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed, self.prefs_ontop_toggled, self.prefs_sticky_toggled, self.prefs_decorated_toggled)
 
 	def prefs_currentoptions_changed(self, entry, _event):
 		if self.config.currentformat != entry.get_text():
@@ -2903,6 +2903,27 @@ class Base(object):
 			self.config.currsongformat2 = entry.get_text()
 			self.update_cursong()
 
+	def prefs_ontop_toggled(self, button):
+		self.config.ontop = button.get_active()
+		if self.window_owner:
+			self.window.set_keep_above(self.config.ontop)
+
+	def prefs_sticky_toggled(self, button):
+		self.config.sticky = button.get_active()
+		if self.window_owner:
+			if self.config.sticky:
+				self.window.stick()
+			else:
+				self.window.unstick()
+
+	def prefs_decorated_toggled(self, button):
+		self.config.decorated = not button.get_active()
+		if self.window_owner:
+			if self.config.decorated != self.window.get_decorated():
+				self.withdraw_app()
+				self.window.set_decorated(self.config.decorated)
+				self.withdraw_app_undo()
+
 	# XXX move the prefs handling parts of prefs_* to preferences.py
 	def prefs_window_response(self, window, response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
 		if response == gtk.RESPONSE_CLOSE:
@@ -2921,19 +2942,6 @@ class Base(object):
 					prefsnotebook.set_current_page(0)
 					direntry.grab_focus()
 					return
-			if self.window_owner:
-				if self.config.ontop:
-					self.window.set_keep_above(True)
-				else:
-					self.window.set_keep_above(False)
-				if self.config.sticky:
-					self.window.stick()
-				else:
-					self.window.unstick()
-				if self.config.decorated != self.window.get_decorated():
-					self.withdraw_app()
-					self.window.set_decorated(self.config.decorated)
-					self.withdraw_app_undo()
 			if self.config.infofile_path != infopath_options.get_text():
 				self.config.infofile_path = os.path.expanduser(infopath_options.get_text())
 				if self.config.use_infofile: self.update_infofile()

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 		
 		self.window = None
 		
-	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed):
+	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed, ontop_toggled, sticky_toggled, decorated_toggled):
 		"""Display the preferences dialog"""
 		self.window = parent_window
 		self.scrobbler = scrobbler
@@ -322,15 +322,13 @@ class Preferences():
 		window_frame.set_shadow_type(gtk.SHADOW_NONE)
 		win_sticky = gtk.CheckButton(_("_Show window on all workspaces"))
 		win_sticky.set_active(self.config.sticky)
-		win_sticky.connect('toggled', self.prefs_config_widget_active, 'sticky')
+		win_sticky.connect('toggled', sticky_toggled)
 		win_ontop = gtk.CheckButton(_("_Keep window above other windows"))
 		win_ontop.set_active(self.config.ontop)
-		win_ontop.connect('toggled', self.prefs_config_widget_active, 'ontop')
+		win_ontop.connect('toggled', ontop_toggled)
 		win_decor = gtk.CheckButton(_("_Hide window titlebar"))
 		win_decor.set_active(not self.config.decorated)
-		win_decor.connect('toggled',
-			lambda w: setattr(self.config, 'decorated',
-				not w.get_active()))
+		win_decor.connect('toggled', decorated_toggled)
 		minimize = gtk.CheckButton(_("_Minimize to system tray on close/escape"))
 		minimize.set_active(self.config.minimize_to_systray)
 		minimize.set_tooltip_text(_("If enabled, closing Sonata will minimize it to the system tray. Note that it's currently impossible to detect if there actually is a system tray, so only check this if you have one."))

multani/sonata

sonata/main.py
@@ -2873,7 +2873,7 @@ class Base(object):
         trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and
                     self.statusicon.get_visible())
                    or (HAVE_EGG and self.trayicon.get_property('visible')))
-        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed)
+        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed, self.prefs_ontop_toggled, self.prefs_sticky_toggled, self.prefs_decorated_toggled)
 
     def prefs_currentoptions_changed(self, entry, _event):
         if self.config.currentformat != entry.get_text():
@@ -2903,6 +2903,27 @@ class Base(object):
             self.config.currsongformat2 = entry.get_text()
             self.update_cursong()
 
+    def prefs_ontop_toggled(self, button):
+        self.config.ontop = button.get_active()
+        if self.window_owner:
+            self.window.set_keep_above(self.config.ontop)
+
+    def prefs_sticky_toggled(self, button):
+        self.config.sticky = button.get_active()
+        if self.window_owner:
+            if self.config.sticky:
+                self.window.stick()
+            else:
+                self.window.unstick()
+
+    def prefs_decorated_toggled(self, button):
+        self.config.decorated = not button.get_active()
+        if self.window_owner:
+            if self.config.decorated != self.window.get_decorated():
+                self.withdraw_app()
+                self.window.set_decorated(self.config.decorated)
+                self.withdraw_app_undo()
+
     # XXX move the prefs handling parts of prefs_* to preferences.py
     def prefs_window_response(self, window, response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
         if response == gtk.RESPONSE_CLOSE:
@@ -2921,19 +2942,6 @@ class Base(object):
                     prefsnotebook.set_current_page(0)
                     direntry.grab_focus()
                     return
-            if self.window_owner:
-                if self.config.ontop:
-                    self.window.set_keep_above(True)
-                else:
-                    self.window.set_keep_above(False)
-                if self.config.sticky:
-                    self.window.stick()
-                else:
-                    self.window.unstick()
-                if self.config.decorated != self.window.get_decorated():
-                    self.withdraw_app()
-                    self.window.set_decorated(self.config.decorated)
-                    self.withdraw_app_undo()
             if self.config.infofile_path != infopath_options.get_text():
                 self.config.infofile_path = os.path.expanduser(infopath_options.get_text())
                 if self.config.use_infofile: self.update_infofile()

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 
         self.window = None
 
-    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed):
+    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed, ontop_toggled, sticky_toggled, decorated_toggled):
         """Display the preferences dialog"""
         self.window = parent_window
         self.scrobbler = scrobbler
@@ -322,15 +322,13 @@ class Preferences():
         window_frame.set_shadow_type(gtk.SHADOW_NONE)
         win_sticky = gtk.CheckButton(_("_Show window on all workspaces"))
         win_sticky.set_active(self.config.sticky)
-        win_sticky.connect('toggled', self.prefs_config_widget_active, 'sticky')
+        win_sticky.connect('toggled', sticky_toggled)
         win_ontop = gtk.CheckButton(_("_Keep window above other windows"))
         win_ontop.set_active(self.config.ontop)
-        win_ontop.connect('toggled', self.prefs_config_widget_active, 'ontop')
+        win_ontop.connect('toggled', ontop_toggled)
         win_decor = gtk.CheckButton(_("_Hide window titlebar"))
         win_decor.set_active(not self.config.decorated)
-        win_decor.connect('toggled',
-            lambda w: setattr(self.config, 'decorated',
-                not w.get_active()))
+        win_decor.connect('toggled', decorated_toggled)
         minimize = gtk.CheckButton(_("_Minimize to system tray on close/escape"))
         minimize.set_active(self.config.minimize_to_systray)
         minimize.set_tooltip_text(_("If enabled, closing Sonata will minimize it to the system tray. Note that it's currently impossible to detect if there actually is a system tray, so only check this if you have one."))