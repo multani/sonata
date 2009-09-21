tuukka/sonata-svn-test

Remove crossfade setting from response event (Stephen Boyd)

sonata/main.py
@@ -2896,10 +2896,10 @@ class Base(object):
 		trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and 
 				    self.statusicon.get_visible())
 				   or (HAVE_EGG and self.trayicon.get_property('visible')))
-		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_window_response, self.prefs_last_tab)
+		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab)
 
 	# XXX move the prefs handling parts of prefs_* to preferences.py
-	def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, crossfadecheck, crossfadespin, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
+	def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
 		if response == gtk.RESPONSE_CLOSE:
 			self.prefs_last_tab = prefsnotebook.get_current_page()
 			if self.config.show_lyrics and self.config.lyrics_location != consts.LYRICS_LOCATION_HOME:
@@ -2945,15 +2945,6 @@ class Base(object):
 					self.withdraw_app()
 					self.window.set_decorated(self.config.decorated)
 					self.withdraw_app_undo()
-			self.config.xfade = crossfadespin.get_value_as_int()
-			if crossfadecheck.get_active():
-				self.config.xfade_enabled = True
-				if self.conn:
-					mpdh.call(self.client, 'crossfade', self.config.xfade)
-			else:
-				self.config.xfade_enabled = False
-				if self.conn:
-					mpdh.call(self.client, 'crossfade', 0)
 			if self.config.infofile_path != infopath_options.get_text():
 				self.config.infofile_path = os.path.expanduser(infopath_options.get_text())
 				if self.config.use_infofile: self.update_infofile()
@@ -2968,6 +2959,17 @@ class Base(object):
 			self.populate_profiles_for_menu()
 			ui.change_cursor(None)
 		window.destroy()
+
+	def prefs_crossfade_changed(self, crossfade_spin):
+		crossfade_value = crossfade_spin.get_value_as_int()
+		mpdh.call(self.client, 'crossfade', crossfade_value)
+
+	def prefs_crossfade_toggled(self, button, crossfade_spin):
+		crossfade_value = crossfade_spin.get_value_as_int()
+		if button.get_active():
+			mpdh.call(self.client, 'crossfade', crossfade_value)
+		else:
+			mpdh.call(self.client, 'crossfade', 0)
 		
 	def prefs_playback_toggled(self, button):
 		self.config.show_playback = button.get_active()

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 		
 		self.window = None
 		
-	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_window_response, prefs_last_tab):
+	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab):
 		"""Display the preferences dialog"""
 		self.window = parent_window
 		self.scrobbler = scrobbler
@@ -202,23 +202,17 @@ class Preferences():
 		crossfadespin.set_numeric(True)
 		crossfadespin.set_increments(1, 5)
 		crossfadespin.set_size_request(70, -1)
+		crossfadespin.connect('value-changed', prefs_crossfade_changed)
 		crossfadelabel2 = ui.label(text=_("Fade length") + ":", x=1)
 		crossfadelabel3 = ui.label(text=_("sec"))
-		if not self.config.xfade_enabled:
-			crossfadespin.set_sensitive(False)
-			crossfadelabel2.set_sensitive(False)
-			crossfadelabel3.set_sensitive(False)
-			crossfadecheck.set_active(False)
-		else:
-			crossfadespin.set_sensitive(True)
-			crossfadelabel2.set_sensitive(True)
-			crossfadelabel3.set_sensitive(True)
-			crossfadecheck.set_active(True)
 		crossfadebox = gtk.HBox()
 		crossfadebox.pack_start(crossfadelabel2)
 		crossfadebox.pack_start(crossfadespin, False, False, 5)
 		crossfadebox.pack_start(crossfadelabel3, False, False, 0)
 		crossfadecheck.connect('toggled', self.prefs_crossfadecheck_toggled, crossfadespin, crossfadelabel2, crossfadelabel3)
+		crossfadecheck.connect('toggled', prefs_crossfade_toggled, crossfadespin)
+		crossfadecheck.set_active(self.config.xfade_enabled)
+		crossfadecheck.toggled() # Force the toggled callback
 		as_table.attach(as_user_label, 0, 1, 0, 1)
 		as_table.attach(as_user_entry, 1, 2, 0, 1)
 		as_table.attach(as_pass_label, 0, 1, 1, 2)
@@ -513,7 +507,7 @@ class Preferences():
 		self.prefswindow.show_all()
 		prefsnotebook.set_current_page(self.last_tab)
 		close_button.grab_focus()
-		self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, crossfadecheck, crossfadespin, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
+		self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
 		# Save previous connection properties to determine if we should try to
 		# connect to MPD after prefs are closed:
 		self.prev_host = self.config.host[self.config.profile_num]

multani/sonata

sonata/main.py
@@ -2896,10 +2896,10 @@ class Base(object):
         trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and
                     self.statusicon.get_visible())
                    or (HAVE_EGG and self.trayicon.get_property('visible')))
-        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_window_response, self.prefs_last_tab)
+        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab)
 
     # XXX move the prefs handling parts of prefs_* to preferences.py
-    def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, crossfadecheck, crossfadespin, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
+    def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
         if response == gtk.RESPONSE_CLOSE:
             self.prefs_last_tab = prefsnotebook.get_current_page()
             if self.config.show_lyrics and self.config.lyrics_location != consts.LYRICS_LOCATION_HOME:
@@ -2945,15 +2945,6 @@ class Base(object):
                     self.withdraw_app()
                     self.window.set_decorated(self.config.decorated)
                     self.withdraw_app_undo()
-            self.config.xfade = crossfadespin.get_value_as_int()
-            if crossfadecheck.get_active():
-                self.config.xfade_enabled = True
-                if self.conn:
-                    mpdh.call(self.client, 'crossfade', self.config.xfade)
-            else:
-                self.config.xfade_enabled = False
-                if self.conn:
-                    mpdh.call(self.client, 'crossfade', 0)
             if self.config.infofile_path != infopath_options.get_text():
                 self.config.infofile_path = os.path.expanduser(infopath_options.get_text())
                 if self.config.use_infofile: self.update_infofile()
@@ -2968,6 +2959,17 @@ class Base(object):
             self.populate_profiles_for_menu()
             ui.change_cursor(None)
         window.destroy()
+
+    def prefs_crossfade_changed(self, crossfade_spin):
+        crossfade_value = crossfade_spin.get_value_as_int()
+        mpdh.call(self.client, 'crossfade', crossfade_value)
+
+    def prefs_crossfade_toggled(self, button, crossfade_spin):
+        crossfade_value = crossfade_spin.get_value_as_int()
+        if button.get_active():
+            mpdh.call(self.client, 'crossfade', crossfade_value)
+        else:
+            mpdh.call(self.client, 'crossfade', 0)
 
     def prefs_playback_toggled(self, button):
         self.config.show_playback = button.get_active()

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 
         self.window = None
 
-    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_window_response, prefs_last_tab):
+    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab):
         """Display the preferences dialog"""
         self.window = parent_window
         self.scrobbler = scrobbler
@@ -202,23 +202,17 @@ class Preferences():
         crossfadespin.set_numeric(True)
         crossfadespin.set_increments(1, 5)
         crossfadespin.set_size_request(70, -1)
+        crossfadespin.connect('value-changed', prefs_crossfade_changed)
         crossfadelabel2 = ui.label(text=_("Fade length") + ":", x=1)
         crossfadelabel3 = ui.label(text=_("sec"))
-        if not self.config.xfade_enabled:
-            crossfadespin.set_sensitive(False)
-            crossfadelabel2.set_sensitive(False)
-            crossfadelabel3.set_sensitive(False)
-            crossfadecheck.set_active(False)
-        else:
-            crossfadespin.set_sensitive(True)
-            crossfadelabel2.set_sensitive(True)
-            crossfadelabel3.set_sensitive(True)
-            crossfadecheck.set_active(True)
         crossfadebox = gtk.HBox()
         crossfadebox.pack_start(crossfadelabel2)
         crossfadebox.pack_start(crossfadespin, False, False, 5)
         crossfadebox.pack_start(crossfadelabel3, False, False, 0)
         crossfadecheck.connect('toggled', self.prefs_crossfadecheck_toggled, crossfadespin, crossfadelabel2, crossfadelabel3)
+        crossfadecheck.connect('toggled', prefs_crossfade_toggled, crossfadespin)
+        crossfadecheck.set_active(self.config.xfade_enabled)
+        crossfadecheck.toggled() # Force the toggled callback
         as_table.attach(as_user_label, 0, 1, 0, 1)
         as_table.attach(as_user_entry, 1, 2, 0, 1)
         as_table.attach(as_pass_label, 0, 1, 1, 2)
@@ -513,7 +507,7 @@ class Preferences():
         self.prefswindow.show_all()
         prefsnotebook.set_current_page(self.last_tab)
         close_button.grab_focus()
-        self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, crossfadecheck, crossfadespin, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
+        self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
         # Save previous connection properties to determine if we should try to
         # connect to MPD after prefs are closed:
         self.prev_host = self.config.host[self.config.profile_num]