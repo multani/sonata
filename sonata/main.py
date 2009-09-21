tuukka/sonata-svn-test

Preferences: format entries apply after losing focus

sonata/main.py
@@ -2873,10 +2873,38 @@ class Base(object):
 		trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and 
 				    self.statusicon.get_visible())
 				   or (HAVE_EGG and self.trayicon.get_property('visible')))
-		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab)
+		self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed)
+
+	def prefs_currentoptions_changed(self, entry, _event):
+		if self.config.currentformat != entry.get_text():
+			self.config.currentformat = entry.get_text()
+			for column in self.current_treeview.get_columns():
+				self.current_treeview.remove_column(column)
+			self.current.initialize_columns()
+			self.current.update_format()
+
+	def prefs_libraryoptions_changed(self, entry, _event):
+		if self.config.libraryformat != entry.get_text():
+			self.config.libraryformat = entry.get_text()
+			self.library.library_browse(root=self.config.wd)
+
+	def prefs_titleoptions_changed(self, entry, _event):
+		if self.config.titleformat != entry.get_text():
+			self.config.titleformat = entry.get_text()
+			self.update_wintitle()
+
+	def prefs_currsongoptions1_changed(self, entry, _event):
+		if self.config.currsongformat1 != entry.get_text():
+			self.config.currsongformat1 = entry.get_text()
+			self.update_cursong()
+
+	def prefs_currsongoptions2_changed(self, entry, _event):
+		if self.config.currsongformat2 != entry.get_text():
+			self.config.currsongformat2 = entry.get_text()
+			self.update_cursong()
 
 	# XXX move the prefs handling parts of prefs_* to preferences.py
-	def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
+	def prefs_window_response(self, window, response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
 		if response == gtk.RESPONSE_CLOSE:
 			self.prefs_last_tab = prefsnotebook.get_current_page()
 			if self.config.show_lyrics and self.config.lyrics_location != consts.LYRICS_LOCATION_HOME:
@@ -2893,22 +2921,6 @@ class Base(object):
 					prefsnotebook.set_current_page(0)
 					direntry.grab_focus()
 					return
-			if self.config.currentformat != currentoptions.get_text():
-				self.config.currentformat = currentoptions.get_text()
-				for column in self.current_treeview.get_columns():
-					self.current_treeview.remove_column(column)
-				self.current.initialize_columns()
-				self.current.update_format()
-			if self.config.libraryformat != libraryoptions.get_text():
-				self.config.libraryformat = libraryoptions.get_text()
-				self.library.library_browse(root=self.config.wd)
-			if self.config.titleformat != titleoptions.get_text():
-				self.config.titleformat = titleoptions.get_text()
-				self.update_wintitle()
-			if (self.config.currsongformat1 != currsongoptions1.get_text()) or (self.config.currsongformat2 != currsongoptions2.get_text()):
-				self.config.currsongformat1 = currsongoptions1.get_text()
-				self.config.currsongformat2 = currsongoptions2.get_text()
-				self.update_cursong()
 			if self.window_owner:
 				if self.config.ontop:
 					self.window.set_keep_above(True)

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 		
 		self.window = None
 		
-	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab):
+	def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed):
 		"""Display the preferences dialog"""
 		self.window = parent_window
 		self.scrobbler = scrobbler
@@ -413,11 +413,11 @@ class Preferences():
 			format_labels.append(label)
 			format_entries.append(entry)
 
-		currentoptions = format_entries[0]
-		libraryoptions = format_entries[1]
-		titleoptions = format_entries[2]
-		currsongoptions1 = format_entries[3]
-		currsongoptions2 = format_entries[4]
+		cbs = (currentoptions_changed, libraryoptions_changed,
+			     titleoptions_changed, currsongoptions1_changed,
+			     currsongoptions2_changed)
+		for entry, cb in zip(format_entries, cbs):
+			entry.connect('focus_out_event', cb)
 
 		availableheading = ui.label(markup='<small>' + _('Available options') + ':</small>', y=0)
 		availablevbox = gtk.VBox()
@@ -545,7 +545,7 @@ class Preferences():
 		self.prefswindow.show_all()
 		prefsnotebook.set_current_page(self.last_tab)
 		close_button.grab_focus()
-		self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
+		self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
 		# Save previous connection properties to determine if we should try to
 		# connect to MPD after prefs are closed:
 		self.prev_host = self.config.host[self.config.profile_num]

multani/sonata

sonata/main.py
@@ -2873,10 +2873,38 @@ class Base(object):
         trayicon_in_use = ((HAVE_STATUS_ICON and self.statusicon.is_embedded() and
                     self.statusicon.get_visible())
                    or (HAVE_EGG and self.trayicon.get_property('visible')))
-        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab)
+        self.preferences.on_prefs_real(self.window, self.popuptimes, self.scrobbler, trayicon_available, trayicon_in_use, self.on_connectkey_pressed, self.on_currsong_notify, self.update_infofile, self.prefs_notif_toggled, self.prefs_stylized_toggled, self.prefs_art_toggled, self.prefs_playback_toggled, self.prefs_progress_toggled, self.prefs_statusbar_toggled, self.prefs_lyrics_toggled, self.prefs_trayicon_toggled, self.prefs_crossfade_toggled, self.prefs_crossfade_changed, self.prefs_window_response, self.prefs_last_tab, self.prefs_currentoptions_changed, self.prefs_libraryoptions_changed, self.prefs_titleoptions_changed, self.prefs_currsongoptions1_changed, self.prefs_currsongoptions2_changed)
+
+    def prefs_currentoptions_changed(self, entry, _event):
+        if self.config.currentformat != entry.get_text():
+            self.config.currentformat = entry.get_text()
+            for column in self.current_treeview.get_columns():
+                self.current_treeview.remove_column(column)
+            self.current.initialize_columns()
+            self.current.update_format()
+
+    def prefs_libraryoptions_changed(self, entry, _event):
+        if self.config.libraryformat != entry.get_text():
+            self.config.libraryformat = entry.get_text()
+            self.library.library_browse(root=self.config.wd)
+
+    def prefs_titleoptions_changed(self, entry, _event):
+        if self.config.titleformat != entry.get_text():
+            self.config.titleformat = entry.get_text()
+            self.update_wintitle()
+
+    def prefs_currsongoptions1_changed(self, entry, _event):
+        if self.config.currsongformat1 != entry.get_text():
+            self.config.currsongformat1 = entry.get_text()
+            self.update_cursong()
+
+    def prefs_currsongoptions2_changed(self, entry, _event):
+        if self.config.currsongformat2 != entry.get_text():
+            self.config.currsongformat2 = entry.get_text()
+            self.update_cursong()
 
     # XXX move the prefs handling parts of prefs_* to preferences.py
-    def prefs_window_response(self, window, response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
+    def prefs_window_response(self, window, response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, prev_host, prev_port, prev_password):
         if response == gtk.RESPONSE_CLOSE:
             self.prefs_last_tab = prefsnotebook.get_current_page()
             if self.config.show_lyrics and self.config.lyrics_location != consts.LYRICS_LOCATION_HOME:
@@ -2893,22 +2921,6 @@ class Base(object):
                     prefsnotebook.set_current_page(0)
                     direntry.grab_focus()
                     return
-            if self.config.currentformat != currentoptions.get_text():
-                self.config.currentformat = currentoptions.get_text()
-                for column in self.current_treeview.get_columns():
-                    self.current_treeview.remove_column(column)
-                self.current.initialize_columns()
-                self.current.update_format()
-            if self.config.libraryformat != libraryoptions.get_text():
-                self.config.libraryformat = libraryoptions.get_text()
-                self.library.library_browse(root=self.config.wd)
-            if self.config.titleformat != titleoptions.get_text():
-                self.config.titleformat = titleoptions.get_text()
-                self.update_wintitle()
-            if (self.config.currsongformat1 != currsongoptions1.get_text()) or (self.config.currsongformat2 != currsongoptions2.get_text()):
-                self.config.currsongformat1 = currsongoptions1.get_text()
-                self.config.currsongformat2 = currsongoptions2.get_text()
-                self.update_cursong()
             if self.window_owner:
                 if self.config.ontop:
                     self.window.set_keep_above(True)

sonata/preferences.py
@@ -48,7 +48,7 @@ class Preferences():
 
         self.window = None
 
-    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab):
+    def on_prefs_real(self, parent_window, popuptimes, scrobbler, trayicon_available, trayicon_in_use, reconnect, renotify, reinfofile, prefs_notif_toggled, prefs_stylized_toggled, prefs_art_toggled, prefs_playback_toggled, prefs_progress_toggled, prefs_statusbar_toggled, prefs_lyrics_toggled, prefs_trayicon_toggled, prefs_crossfade_toggled, prefs_crossfade_changed, prefs_window_response, prefs_last_tab, currentoptions_changed, libraryoptions_changed, titleoptions_changed, currsongoptions1_changed, currsongoptions2_changed):
         """Display the preferences dialog"""
         self.window = parent_window
         self.scrobbler = scrobbler
@@ -413,11 +413,11 @@ class Preferences():
             format_labels.append(label)
             format_entries.append(entry)
 
-        currentoptions = format_entries[0]
-        libraryoptions = format_entries[1]
-        titleoptions = format_entries[2]
-        currsongoptions1 = format_entries[3]
-        currsongoptions2 = format_entries[4]
+        cbs = (currentoptions_changed, libraryoptions_changed,
+                 titleoptions_changed, currsongoptions1_changed,
+                 currsongoptions2_changed)
+        for entry, cb in zip(format_entries, cbs):
+            entry.connect('focus_out_event', cb)
 
         availableheading = ui.label(markup='<small>' + _('Available options') + ':</small>', y=0)
         availablevbox = gtk.VBox()
@@ -545,7 +545,7 @@ class Preferences():
         self.prefswindow.show_all()
         prefsnotebook.set_current_page(self.last_tab)
         close_button.grab_focus()
-        self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, currentoptions, libraryoptions, titleoptions, currsongoptions1, currsongoptions2, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
+        self.prefswindow.connect('response', prefs_window_response, prefsnotebook, direntry, infopath_options, using_mpd_env_vars, self.prev_host, self.prev_port, self.prev_password)
         # Save previous connection properties to determine if we should try to
         # connect to MPD after prefs are closed:
         self.prev_host = self.config.host[self.config.profile_num]