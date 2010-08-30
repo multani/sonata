fd950762244e517d6d5b32ea04ac3c85b4cbb710
Paco <ribemont.francois@gmail.com>
2010-08-30 17:52:13 +0100

    Changes on file mpdhelper.py: PEP8 compliant

--- sonata/mpdhelper.py
+++ sonata/mpdhelper.py
@@ -1,13 +1,17 @@
 
-import locale, sys, os
+import locale
+import sys
+import os
 from time import strftime
 from misc import remove_list_duplicates
 
 suppress_errors = False
 
+
 def suppress_mpd_errors(val):
     global suppress_errors
     suppress_errors = val
+
 
 def status(client):
     result = call(client, 'status')
@@ -16,8 +20,10 @@ def status(client):
     else:
         return {}
 
+
 def currsong(client):
     return call(client, 'currentsong')
+
 
 def get(mapping, key, alt='', *sanitize_args):
     """Get a value from a mpd song and sanitize appropriately.
@@ -35,6 +41,7 @@ def get(mapping, key, alt='', *sanitize_args):
         value = value[0]
     return _sanitize(value, *sanitize_args) if sanitize_args else value
 
+
 def _sanitize(tag, return_int=False, str_padding=0):
     # Sanitizes a mpd tag; used for numerical tags. Known forms
     # for the mpd tag can be "4", "4/10", and "4,10".
@@ -46,11 +53,13 @@ def _sanitize(tag, return_int=False, str_padding=0):
 
     return tag.zfill(str_padding)
 
+
 def conout(s):
     # A kind of 'print' which does not throw exceptions if the string
     # to print cannot be converted to console encoding; instead it
     # does a "readable" conversion
     print s.encode(locale.getpreferredencoding(), "replace")
+
 
 def call(mpdclient, mpd_cmd, *mpd_args):
     try:
@@ -58,13 +67,15 @@ def call(mpdclient, mpd_cmd, *mpd_args):
     except:
         if not mpd_cmd in ['disconnect', 'lsinfo', 'listplaylists']:
             if not suppress_errors:
-                print strftime("%Y-%m-%d %H:%M:%S") + "  " + str(sys.exc_info()[1])
+                print '%s  %s' % (strftime("%Y-%m-%d %H:%M:%S"),
+                                  str(sys.exc_info()[1]))
         if mpd_cmd in ['lsinfo', 'list']:
             return []
         else:
             return None
 
     return retval
+
 
 def mpd_major_version(client):
     try:
@@ -74,8 +85,10 @@ def mpd_major_version(client):
     except:
         return 0.0
 
+
 def mpd_is_updating(status):
     return status and status.get('updating_db', 0)
+
 
 def update(mpdclient, paths, status):
     # mpd 0.14.x limits the number of paths that can be
