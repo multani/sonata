ca7f023c7c0cda1dcb2238a0dfa4cf54b045c645
Jörg Thalheim <jthalheim@gmail.com>
2012-02-18 10:07:33 +0100

    mpdhelper.py include self in __set_supress_errors
    MPDHelper is an instance method so we need to include the object as well
    in the parameter list.

--- sonata/mpdhelper.py
+++ sonata/mpdhelper.py
@@ -7,13 +7,12 @@ import os
 from time import strftime
 from sonata.misc import remove_list_duplicates
 
-
 class MPDHelper(object):
     def __init__(self, client):
         self._client = client
         self.logger = logging.getLogger(__name__)
 
-    def __set_suppress_errors(suppress_errors):
+    def __set_suppress_errors(self, suppress_errors):
         if suppress_errors:
             # Well, maybe we still want some very bad errors, who knows
             self.logger.setLevel(logging.CRITICAL)
