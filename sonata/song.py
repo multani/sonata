b34570a916fc975c56eb473922201384c91e74aa
Jörg Thalheim <jthalheim@gmail.com>
2012-02-19 09:17:12 +0100

    song.py SongRecord: more general comparision

--- sonata/song.py
+++ sonata/song.py
@@ -1,5 +1,5 @@
 
-class SongRecord():
+class SongRecord(object):
     """A convenient class to store a song"""
     # use slots for memory efficiency
     __slots__ = ['album', 'artist', 'genre', 'year', 'path']
@@ -15,21 +15,21 @@ class SongRecord():
             self.album, self.artist, self.genre, self.year, self.path)
     def __key(self):
         return (self.album, self.artist, self.genre, self.year, self.path)
-    def __lt__(self, other):
-        return self.__key() <  other.__key()
-    def __le__(self, other):
-        return self.__key() <=  other.__key()
-    def __eq__(self, other):
-        return isinstance(other, SongRecord) and self.__key() == other.__key()
-    def __ne__(self, other):
-        return not isinstance(other, SongRecord) or self.__key() != other.__key()
-    def __gt__(self, other):
-        return self.__key() > other.__key()
-    def __ge__(self, other):
-        return self.__key() >= other.__key()
     def __iter__(self):
         """Make the classed iterable. useful for unpacking variables"""
         for i in (self.album, self.artist, self.genre, self.year, self.path):
             yield i
     def __hash__(self):
         return hash(self.__key())
+    def __lt__(self, other):
+        return self.__key() <  other
+    def __le__(self, other):
+        return self.__key() <=  other
+    def __eq__(self, other):
+        return self.__key() == other
+    def __ne__(self, other):
+        return self.__key() != other
+    def __gt__(self, other):
+        return self.__key() > other
+    def __ge__(self, other):
+        return self.__key() >= other
