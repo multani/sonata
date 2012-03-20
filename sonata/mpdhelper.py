multani/sonata

commit eb8bff6dadb23d3424557b9336bda410ebbd6eba

Refactor MPDHelper.call and __getattr__
MPDHelper throw AttributeError as expected on unknown commands.
All commands are wrapped once into a logging function.

sonata/mpdhelper.py
@@ -6,6 +6,7 @@ import sys
 import os
 from time import strftime
 from misc import remove_list_duplicates
+from mpd import MPDError
 
 
 class MPDHelper(object):
@@ -23,41 +24,34 @@ class MPDHelper(object):
     suppress_errors = property(fset=__set_suppress_errors)
 
     def __getattr__(self, attr):
-        """Catch-all for methods with no special implementation."""
-        # XXX we still pass through the .call() method, since the original code
-        # expected this, and this method does some additionnal postprocessing in
-        # case of error. If .call() is cleaned up, maybe we can somehow merge
-        # .__getattr__() and .call() together.
-        return functools.partial(self.call, attr)
+        """
+        Wraps all calls from mpd client into a proper function,
+        which catches all MPDClient related exceptions and log them.
+        """
+        cmd = getattr(self._client, attr)
+        # save result, so function have to be constructed only once
+        wrapped_cmd = functools.partial(self.call, cmd, attr)
+        setattr(self, attr, wrapped_cmd)
+        return wrapped_cmd
 
-    def call(self, command, *args):
-        # This is potentially called (too) many times. In the cas the logging is not
+    def call(self, cmd, cmd_name, *args):
+        # This is potentially called (too) many times. In the case the logging is not
         # active, just don't try to do anything at all. This is supposed to save
         # some performance in the case it is not active.
         if self.logger.isEnabledFor(logging.DEBUG):
-            self.logger.debug("Calling MPD %s%r", command, args)
-
+            self.logger.debug("Calling MPD %s%r", cmd_name, args)
         try:
-            retval = getattr(self._client, command)(*args)
-        except Exception, e:
-            # XXX make the distinction between bad getattr() call and bad MPD
-            # call?
-            if not command in ['disconnect', 'lsinfo', 'listplaylists']:
-                self.logger.error("%s", e)
-            if command in ['lsinfo', 'list']:
+            return cmd(*args)
+        except MPDError, e:
+            if cmd_name in ['lsinfo', 'list']:
+                # return sane values, which could be used afterwards
                 return []
-            else:
+            elif cmd_name == 'disconnect':
+                # We really don't care, if connections breaks, before we
+                # could disconnect.
                 return None
-
-        return retval
-
-    def status(self):
-        result = self.call('status')
-        # XXX why we return different things here?
-        if result and 'state' in result:
-            return result
-        else:
-            return {}
+            else:
+                self.logger.error("%s", e)
 
     @property
     def version(self):

sonata/cli.py
@@ -188,15 +188,13 @@ class CliMain(object):
     def _execute_prev(self):
         self.mpd.previous()
 
-    def _execute_bool(self, cmd):
-        """Set the reverse the value of cmd"""
-        self.mpd.call(cmd, int(not int(self.status[cmd])))
-
     def _execute_random(self):
-        self._execute_bool('random')
+        opt = int(not int(self.status["random"]))
+        self.mpd.random(opt)
 
     def _execute_repeat(self):
-        self._execute_bool('repeat')
+        opt = int(not int(self.status["repeat"]))
+        self.mpd.repeat(opt)
 
     def _execute_pp(self):
         if self.status['state'] in ['play']:

sonata/main.py
@@ -3017,16 +3017,13 @@ class Base(object):
             self.mpd.clear()
             self.iterate_now()
 
-    def _toggle_clicked(self, command, widget):
-        self.mpd.call(command, int(widget.get_active()))
-
     def on_repeat_clicked(self, widget):
         if self.conn:
-            self._toggle_clicked('repeat', widget)
+            self.mpd.repeat(int(widget.get_active()))
 
     def on_random_clicked(self, widget):
         if self.conn:
-            self._toggle_clicked('random', widget)
+            self.mpd.random(int(widget.get_active()))
 
     def setup_prefs_callbacks(self):
         extras = preferences.Extras_cbs

commit d8982b3302a8aa19c0beb864f848523fffcc729b

Don't crash if Sonata can't connect to MPD at startup.
This fix a regression introduced by eb8bff6: previously, Sonata was catching
all the exception when calling MPD. It has been changed to catch every MPDError instead
but python-mpd raises a socket.error if the connection fails.

We now handle both MPD-related errors, and the connection failed errors.

(I also removed some unused imports).

sonata/mpdhelper.py
@@ -1,12 +1,13 @@
 
 import functools
-import locale
 import logging
-import sys
 import os
-from time import strftime
-from sonata.misc import remove_list_duplicates
+import socket
+
 from mpd import MPDError
+
+from sonata.misc import remove_list_duplicates
+
 
 class MPDHelper(object):
     def __init__(self, client):
@@ -41,7 +42,7 @@ class MPDHelper(object):
             self.logger.debug("Calling MPD %s%r", cmd_name, args)
         try:
             return cmd(*args)
-        except MPDError as e:
+        except (socket.error, MPDError) as e:
             if cmd_name in ['lsinfo', 'list']:
                 # return sane values, which could be used afterwards
                 return []