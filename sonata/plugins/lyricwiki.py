95e82fffda47976b81ce3cf0abbf8fc4ab316587
Anton Lashkov <lenton_91@mail.ru>
2012-08-21 20:47:54 +0400

    Update plugin. Remove class. Simplification.

--- sonata/plugins/lyricwiki.py
+++ sonata/plugins/lyricwiki.py
@@ -3,53 +3,38 @@
 # [plugin]
 # plugin_format: 0, 0
 # name: LyricWiki
-# version: 0, 1, 0
+# version: 0, 2, 0
 # description: Fetch lyrics from lyrics.wikia.com.
 # author: Anton Lashkov
 # author_email: Lenton_91@mail.ru
 # url:
 # license: GPL v3 or later
 # [capabilities]
-# enablables: on_enable
 # lyrics_fetching: get_lyrics
 ### END PLUGIN INFO
 
-
 import urllib
 
-lyricwiki = None
+def lyricwiki_format(text):
+    return urllib.quote(str(unicode(text).title()))
 
-class LyricWiki(object):
+def get_lyrics(search_artist, search_title):
+    addr = 'http://lyrics.wikia.com/index.php?title=%s:%s&action=edit' % (
+        lyricwiki_format(search_artist), lyricwiki_format(search_title))
 
-    def __init__(self):
-        pass
+    try:
+        content = urllib.urlopen(addr).read()
+        lyrics = content.split("&lt;lyrics>")[1].split("&lt;/lyrics>")[0].strip()
 
-    def lyricwiki_format(self, text):
-        return urllib.quote(str(unicode(text).title()))
-
-    def get_lyrics(self, search_artist, search_title):
-
-        try:
-            addr = 'http://lyrics.wikia.com/index.php?title=%s:%s&action=edit' % (self.lyricwiki_format(search_artist), self.lyricwiki_format(search_title))
-            content = urllib.urlopen(addr).read()
-
-            lyrics = content.split("&lt;lyrics>")[1].split("&lt;/lyrics>")[0].strip()
-            if lyrics != ("&lt;!-- PUT LYRICS HERE (and delete this entire line) -->") and lyrics != "":
-                return lyrics.decode("utf-8")
-            else:
-                return None
-        except Exception:
+        if lyrics != \
+           ("&lt;!-- PUT LYRICS HERE (and delete this entire line) -->") \
+        and lyrics != "":
+            return lyrics.decode("utf-8")
+        else:
             return None
 
+    except Exception:
+        return None
 
-def on_enable(state):
-
-    global lyricwiki
-
-    if state:
-        if lyricwiki is None:
-            lyricwiki = LyricWiki()
-
-def get_lyrics(artist, title):
-
-    return lyricwiki.get_lyrics(artist, title)
+if __name__ == "__main__":
+    print get_lyrics("anti-flag", "Death Of A Nation")
