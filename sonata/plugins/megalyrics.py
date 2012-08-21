f16c83920a4b4dd426052ea8244033c977606a22
Anton Lashkov <lenton_91@mail.ru>
2012-08-21 20:47:39 +0400

    Update plugin. Remove class. Simplification.

--- sonata/plugins/megalyrics.py
+++ sonata/plugins/megalyrics.py
@@ -1,61 +1,50 @@
+# -*- coding: utf-8 -*-
 
 ### BEGIN PLUGIN INFO
 # [plugin]
 # plugin_format: 0, 0
 # name: Megalyrics
-# version: 0, 1, 0
+# version: 0, 2, 0
 # description: Fetch lyrics from megalyrics.ru
 # author: Anton Lashkov
 # author_email: Lenton_91@mail.ru
 # url:
 # license: GPL v3 or later
 # [capabilities]
-# enablables: on_enable
 # lyrics_fetching: get_lyrics
 ### END PLUGIN INFO
 
 import re
 import urllib
 
-megalyrics = None
+def clear(string):
+    #It should make "when-you-don-t-control-your-government-people-want-to-kill-you"
+    #from "When You Don't Control Your Government, People Want to Kill You"
 
-class Megalyrics():
-    def __init__(self):
-        pass
+    string = str(string).lower()
+    string = re.sub(r"[\,\?\!\:\;\$\(\)]",'',string)
+    string = re.sub(r"[\'\.\ ]",'-',string)
 
-    def clear(self, string):
-        #It should make "when-you-don-t-control-your-government-people-want-to-kill-you"
-        #from "When You Don't Control Your Government, People Want to Kill You"
+    return string
 
-        string = str(string).lower()
-        string = re.sub(r"[\,\?\!\:\;\$\(\)]",'',string)
-        string = re.sub(r"[\'\.\ ]",'-',string)
+def get_lyrics(search_artist, search_title):
+    addr = "http://megalyrics.ru/lyric/%s/%s.htm" % (clear(search_artist),
+                                                     clear(search_title))
 
-        return string
+    try:
+        page = urllib.urlopen(addr).read()
+        lyrics = page.split('<pre class="lyric"><h2>Текст песни</h2>')[1].\
+                 split("</pre>")[0].strip()
 
-    def get_lyrics(self, search_artist, search_title):
+        lyrics = lyrics.replace("<br/>","\n")
 
-        addr = "http://megalyrics.ru/lyric/%s/%s.htm" % (self.clear(search_artist), self.clear(search_title))
-
-        try:
-            page = urllib.urlopen(addr).read()
-            lyrics = page.split('<pre class="lyric">')[1].split("</pre>")[0].strip()
-
-            if lyrics == "":
-                return None
-        except:
+        if lyrics == "":
             return None
+        else:
+            return lyrics
 
-        return lyrics
+    except:
+        return None
 
-def on_enable(state):
-
-    global megalyrics
-
-    if state:
-        if megalyrics is None:
-            megalyrics = Megalyrics()
-
-def get_lyrics(artist, title):
-
-    return megalyrics.get_lyrics(artist, title)
+if __name__ == "__main__":
+    print get_lyrics("anti-flag", "Death Of A Nation")
