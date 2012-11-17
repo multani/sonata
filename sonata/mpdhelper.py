05c406f117dbc25eac8441802a5d1ffd688c03ee
Jonathan Ballet <jon@multani.info>
2012-11-17 15:00:12 +0800

    Remove .mpdhelper.get() and _sanitize() functions

--- sonata/mpdhelper.py
+++ sonata/mpdhelper.py
@@ -176,42 +176,6 @@ class SongResult(object):
         return self._mapping.get('file', '') # XXX should be always here?
 
 
-def get(mapping, key, alt='', *sanitize_args):
-    """Get a value from a mpd song and sanitize appropriately.
-
-    sanitize_args: Arguments to pass to sanitize
-
-    If the value is a list, only the first element is returned.
-    Examples:
-        get({'baz':['foo', 'bar']}, 'baz', '') -> 'foo'
-        get({'baz':34}, 'baz', '', True) -> 34
-    """
-
-    value = mapping.get(key, alt)
-    if isinstance(value, list):
-        value = value[0]
-    return _sanitize(value, *sanitize_args) if sanitize_args else value
-
-
-def _sanitize(tag, return_int=False, str_padding=0):
-    # Sanitizes a mpd tag; used for numerical tags. Known forms
-    # for the mpd tag can be "4", "4/10", and "4,10".
-    if not tag:
-        return tag
-    tag = str(tag).replace(',', ' ', 1).replace('/', ' ', 1).split()
-
-    # fix #2842: tag only consist of '/' or ','
-    if len(tag) == 0:
-        tag = ''
-    else:
-        tag = tag[0]
-
-    if return_int:
-        return int(tag) if tag.isdigit() else 0
-
-    return tag.zfill(str_padding)
-
-
 # XXX to be move when we can handle status change in the main interface
 def mpd_is_updating(status):
     return status and status.get('updating_db', 0)
