multani/sonata

commit b242a39eef951a317ae361b15046c1a976f507b0

fix obivious python3 incompatibilities

sonata/cli.py
@@ -223,27 +223,27 @@ class CliMain(object):
             try:
                 time = misc.convert_time(mpdh.get(self.songinfo, 'time',
                                                   '', True))
-                print "%s: %s/%s" % (_("Time"), at_time, time)
+                print("%s: %s/%s" % (_("Time"), at_time, time))
             except:
-                print "%s: %s" % (_("Time"), at_time)
-            print "%s: %s" % (_("Bitrate"),
-                      self.status.get('bitrate', ''))
+                print("%s: %s" % (_("Time"), at_time))
+            print("%s: %s" % (_("Bitrate"),
+                      self.status.get('bitrate', '')))
         else:
-            print _("MPD stopped")
+            print(_("MPD stopped"))
 
     def _execute_status(self):
         state_map = {
                 'play': _("Playing"),
                 'pause': _("Paused"),
                 'stop': _("Stopped")}
-        print "%s: %s" % (_("State"),
+        print("%s: %s" % (_("State")),
                 state_map[self.status['state']])
 
-        print "%s %s" % (_("Repeat:"), _("On") \
-                         if self.status['repeat'] == '1' else _("Off"))
-        print "%s %s" % (_("Random:"), _("On") \
-                         if self.status['random'] == '1' else _("Off"))
-        print "%s: %s/100" % (_("Volume"), self.status['volume'])
-        print "%s: %s %s" % (_('Crossfade'), self.status['xfade'],
+        print("%s %s" % (_("Repeat:"), _("On") \
+                         if self.status['repeat'] == '1' else _("Off")))
+        print("%s %s" % (_("Random:"), _("On") \
+                         if self.status['random'] == '1' else _("Off")))
+        print("%s: %s/100" % (_("Volume"), self.status['volume']))
+        print("%s: %s %s" % (_('Crossfade'), self.status['xfade'],
                              gettext.ngettext('second', 'seconds',
-                                              int(self.status['xfade'])))
+                                              int(self.status['xfade']))))

sonata/about.py
@@ -172,17 +172,17 @@ class About(object):
             statslabel += '%s %s.' % (days_of_playtime,
                                       gettext.ngettext('day of bliss',
                                                        'days of bliss',
-                                                       int(days_of_playtime)))
+                                                       float(days_of_playtime)))
         else:
             statslabel += '%s %s.' % (hours_of_playtime,
                                       gettext.ngettext('hour of bliss',
                                                        'hours of bliss',
-                                                       int(hours_of_playtime)))
+                                                       float(hours_of_playtime)))
 
         return statslabel
 
     def about_load(self, stats):
-        self.about_dialog = gtk.AboutDialog()
+        self.about_dialog = Gtk.AboutDialog()
         try:
             self.about_dialog.set_transient_for(self.parent_window)
             self.about_dialog.set_modal(True)

sonata/artwork.py
@@ -276,8 +276,8 @@ class Artwork(object):
         for row in self.lib_model:
             artist, album, path = library_get_data(row[1], 'artist', 'album',
                                                    'path')
-            if unicode(play_artist).lower() == unicode(artist).lower() \
-            and unicode(play_album).lower() == unicode(album).lower():
+            if str(play_artist).lower() == str(artist).lower() \
+            and str(play_album).lower() == str(album).lower():
                 pb = self.get_library_artwork_cached_pb(cache_key, None)
                 self.lib_model.set_value(row.iter, 0, pb)
 

sonata/audioscrobbler.py
@@ -303,10 +303,10 @@ audioscrobbler_request_version = '1.0'
 audioscrobbler_request_host = 'ws.audioscrobbler.com'
 
 # AudioScrobblerPost configuration settings
-audioscrobbler_post_version = u'1.2' #updated
+audioscrobbler_post_version = '1.2' #updated
 audioscrobbler_post_host = 'post.audioscrobbler.com'
-client_name = u'sna'
-pyscrobbler_version = u'1.0.0.0' # This is set to 1.0 while we use
+client_name = 'sna'
+pyscrobbler_version = '1.0.0.0' # This is set to 1.0 while we use
                              # client_name = u'tst' as we keep getting
                              # UPDATE responses with anything less.
 
@@ -495,7 +495,7 @@ class AudioScrobblerQuery:
             # Open the URL and test the response
             try:
                 response = urllib2.urlopen(url)
-            except urllib2.HTTPError, error:
+            except urllib2.HTTPError as error:
                 if error.code == 304:
                     return AudioScrobblerItem(cache[url].getroot(), _self, url)
                 if error.code == 400:
@@ -503,7 +503,7 @@ class AudioScrobblerQuery:
                                                         error.fp.read())
                 raise AudioScrobblerConnectionError('http', error.code,
                                                     error.msg)
-            except urllib2.URLError, error:
+            except urllib2.URLError as error:
                 code = error.reason.args[0]
                 message = error.reason.args[1]
                 raise AudioScrobblerConnectionError('network', code, message)
@@ -596,8 +596,8 @@ class AudioScrobblerPost:
     #Updated to protocol version 1.2
 
     def __init__(self,
-                 username=u'',
-                 md5_password=u'',
+                 username='',
+                 md5_password='',
                  client_name=client_name,
                  client_version=pyscrobbler_version,
                  protocol_version=audioscrobbler_post_version,
@@ -655,10 +655,10 @@ class AudioScrobblerPost:
         req = urllib2.Request(url)
         try:
             url_handle = urllib2.urlopen(req)
-        except urllib2.HTTPError, error:
+        except urllib2.HTTPError as error:
             self.authenticated = False
             raise AudioScrobblerConnectionError('http', error.code, error.msg)
-        except urllib2.URLError, error:
+        except urllib2.URLError as error:
             self.authenticated = False
             code = error.reason#.args[0]
             message = error.reason#.args[1]
@@ -716,10 +716,10 @@ class AudioScrobblerPost:
                   song_title,
                   length,
                   date_played,
-                  tracknumber=u'',
-                  album=u'',
-                  mbid=u'',
-                  source=u'P'):
+                  tracknumber='',
+                  album='',
+                  mbid='',
+                  source='P'):
 
         """
         Add the track to the local cache, and try to post it to the server
@@ -742,10 +742,10 @@ class AudioScrobblerPost:
                  song_title,
                  length,
                  date_played,
-                 tracknumber=u'',
-                 album=u'',
-                 mbid=u'',
-                 source=u'P'):
+                 tracknumber='',
+                 album='',
+                 mbid='',
+                 source='P'):
 
         """ Add a track to the local cache """
 
@@ -771,7 +771,7 @@ class AudioScrobblerPost:
                         'i[%s]': date_played,
                         'b[%s]': album.encode('utf8'),
                         'm[%s]': mbid.encode('utf8'),
-                        'r[%s]': u'',
+                        'r[%s]': '',
                         'n[%s]': tracknumber.encode('utf8'),
                         'o[%s]': source.encode('utf8'),
                         }
@@ -782,7 +782,7 @@ class AudioScrobblerPost:
                         'i[%s]': date_played,
                         'b[%s]': album,
                         'm[%s]': mbid.encode('utf8'),
-                        'r[%s]': u'',
+                        'r[%s]': '',
                         'n[%s]': tracknumber.encode('utf8'),
                         'o[%s]': source.encode('utf8'),
                         }
@@ -791,10 +791,10 @@ class AudioScrobblerPost:
     def nowplaying(self,
                   artist_name,
                   song_title,
-                  length=u'',
-                  tracknumber=u'',
-                  album=u'',
-                  mbid=u''):
+                  length='',
+                  tracknumber='',
+                  album='',
+                  mbid=''):
 
         self.auth()
 
@@ -821,7 +821,7 @@ class AudioScrobblerPost:
 
         try:
             url_handle = urllib2.urlopen(req)
-        except urllib2.HTTPError, error:
+        except urllib2.HTTPError as error:
             self.authenticated = False
             self.logger.error(
                 "%s", AudioScrobblerConnectionError('http', error.code, error.msg))
@@ -870,9 +870,9 @@ class AudioScrobblerPost:
 
         try:
             url_handle = urllib2.urlopen(req)
-        except urllib2.HTTPError, error:
+        except urllib2.HTTPError as error:
             raise AudioScrobblerConnectionError('http', error.code, error.msg)
-        except urllib2.URLError, error:
+        except urllib2.URLError as error:
             args = getattr(error.reason, 'args', None)
             code = '000'
             message = str(error)
@@ -918,7 +918,7 @@ class AudioScrobblerPost:
 
         """ Save all tracks in the cache to a file """
 
-        conf = ConfigParser.ConfigParser()
+        conf = configparser.ConfigParser()
 
         # Save each track in cache:
         count = 0
@@ -943,7 +943,7 @@ class AudioScrobblerPost:
         if not os.path.isfile(filename):
             return
 
-        conf = ConfigParser.ConfigParser()
+        conf = configparser.ConfigParser()
         try:
             conf.read(filename)
         except IOError:

sonata/library.py
@@ -548,11 +548,11 @@ class Library(object):
                     name = mpdh.get(item, 'directory').split('/')[-1]
                     data = self.library_set_data(path=mpdh.get(item,
                                                                'directory'))
-                    bd += [('d' + unicode(name).lower(), [self.openpb, data,
+                    bd += [('d' + str(name).lower(), [self.openpb, data,
                                                       misc.escape_html(name)])]
                 elif 'file' in item:
                     data = self.library_set_data(path=mpdh.get(item, 'file'))
-                    bd += [('f' + unicode(mpdh.get(item, 'file')).lower(),
+                    bd += [('f' + str(mpdh.get(item, 'file')).lower(),
                             [self.sonatapb, data,
                              formatting.parse(self.config.libraryformat, item,
                                               True)])]
@@ -679,16 +679,16 @@ class Library(object):
                 break
             VA = False
             for j in range(1, consts.NUM_ARTISTS_FOR_VA):
-                if unicode(self.library_get_data(albums[i], 'album')).lower() \
-                   != unicode(self.library_get_data(albums[i + j],
+                if str(self.library_get_data(albums[i], 'album')).lower() \
+                   != str(self.library_get_data(albums[i + j],
                                                     'album')).lower() or \
                    self.library_get_data(albums[i], 'year') != \
                    self.library_get_data(albums[i + j], 'year') or \
                    self.library_get_data(albums[i], 'path') != \
                    self.library_get_data(albums[i + j], 'path'):
                     break
-                if unicode(self.library_get_data(albums[i], 'artist')) == \
-                   unicode(self.library_get_data(albums[i + j], 'artist')):
+                if str(self.library_get_data(albums[i], 'artist')) == \
+                   str(self.library_get_data(albums[i + j], 'artist')):
                     albums.pop(i + j)
                     break
 
@@ -702,9 +702,9 @@ class Library(object):
                                                   year=year, path=path)
                 j = 1
                 while i + j <= len(albums) - 1:
-                    if unicode(self.library_get_data(albums[i],
+                    if str(self.library_get_data(albums[i],
                                                      'album')).lower() == \
-                       unicode(self.library_get_data(albums[i + j],
+                       str(self.library_get_data(albums[i + j],
                                                      'album')).lower() \
                     and self.library_get_data(albums[i], 'year') == \
                        self.library_get_data(albums[i + j], 'year'):
@@ -829,7 +829,7 @@ class Library(object):
                                                    song, True)])]
             except:
                 bd += [('f' + disc + track + \
-                        unicode(mpdh.get(song, 'file')).lower(),
+                        str(mpdh.get(song, 'file')).lower(),
                         [self.sonatapb, data,
                          formatting.parse(self.config.libraryformat, song,
                                           True)])]
@@ -916,7 +916,7 @@ class Library(object):
                     # This allows us to match untagged items
                     cached_list.append('')
                 for item in cached_list:
-                    if unicode(item).lower() == unicode(search).lower():
+                    if str(item).lower() == str(search).lower():
                         itemlist.append(item)
             if len(itemlist) == 0:
                 # There should be no results!
@@ -1027,8 +1027,8 @@ class Library(object):
                         # "foobar" isn't returned too
                         for arg in args_tuple[::2]:
                             if arg in item and \
-                               unicode(mpdh.get(item, arg)).upper() != \
-                               unicode(args_tuple[pos + 1]).upper():
+                               str(mpdh.get(item, arg)).upper() != \
+                               str(args_tuple[pos + 1]).upper():
                                 match = False
                                 break
                             pos += 2
@@ -1398,7 +1398,7 @@ class Library(object):
             for row in self.prevlibtodo_base_results:
                 is_match = True
                 for regexp in regexps:
-                    if not regexp.match(unicode(mpdh.get(row,
+                    if not regexp.match(str(mpdh.get(row,
                                                          searchby)).lower()):
                         is_match = False
                         break
@@ -1409,7 +1409,7 @@ class Library(object):
                 allstr = " ".join(mpdh.get(row, meta) for meta in row)
                 is_match = True
                 for regexp in regexps:
-                    if not regexp.match(unicode(allstr).lower()):
+                    if not regexp.match(str(allstr).lower()):
                         is_match = False
                         break
                 if is_match:

sonata/main.py
@@ -2430,9 +2430,9 @@ class Base(object):
                 # right one. If we have a VA album, compare paths. Otherwise,
                 # compare artists.
                 for dataitem in datalist:
-                    if unicode(
+                    if str(
                         library.library_get_data(dataitem, 'artist')).lower() \
-                       == unicode(mpdh.get(self.songinfo, 'artist')).lower() \
+                       == str(mpdh.get(self.songinfo, 'artist')).lower() \
                     or (library.library_get_data(dataitem, 'artist') == \
                         self.library.get_VAstr() and \
                         library.library_get_data(dataitem, 'path') == \
@@ -2444,16 +2444,16 @@ class Base(object):
             # Find all songs in album:
             retsongs = []
             for song in songs:
-                if unicode(mpdh.get(song, 'album')).lower() == \
-                   unicode(library.library_get_data(datalist[0],
+                if str(mpdh.get(song, 'album')).lower() == \
+                   str(library.library_get_data(datalist[0],
                                                     'album')).lower() \
                 and mpdh.get(song, 'date', None) == \
                    library.library_get_data(datalist[0], 'year'):
                     if library.library_get_data(datalist[0], 'artist') == \
                        self.library.get_VAstr() \
-                    or unicode(library.library_get_data(datalist[0],
+                    or str(library.library_get_data(datalist[0],
                                                         'artist')).lower() == \
-                       unicode(mpdh.get(song, 'artist')).lower():
+                       str(mpdh.get(song, 'artist')).lower():
                         retsongs.append(song)
 
             artist = library.library_get_data(datalist[0], 'artist')

sonata/misc.py
@@ -125,7 +125,6 @@ the_re = re.compile('^the ')
 
 
 def lower_no_the(s):
-    s = unicode(s)
     s = the_re.sub('', s.lower())
     s = str(s)
     return s

sonata/mpdhelper.py
@@ -39,7 +39,7 @@ class MPDHelper(object):
 
         try:
             retval = getattr(self._client, command)(*args)
-        except Exception, e:
+        except Exception as e:
             # XXX make the distinction between bad getattr() call and bad MPD
             # call?
             if not command in ['disconnect', 'lsinfo', 'listplaylists']:

commit 9ecb7f53868da4a99e1c6848307f2989a5d31f42

cli: fix result of "status", broken since b242a39e

sonata/cli.py
@@ -233,8 +233,7 @@ class CliMain(object):
                 'play': _("Playing"),
                 'pause': _("Paused"),
                 'stop': _("Stopped")}
-        print("%s: %s" % (_("State")),
-                state_map[self.status['state']])
+        print("%s: %s" % (_("State"), state_map[self.status['state']]))
 
         print("%s %s" % (_("Repeat:"), _("On") \
                          if self.status['repeat'] == '1' else _("Off")))