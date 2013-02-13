f0c54d11a344480db78564722c5ea6f5a8e405e2
Jonathan Ballet <jon@multani.info>
2013-02-13 18:22:26 +0100

    lyrics: follow redirect pages on LyricsWiki
    Some pages in LyricsWiki actually redirect to another page to find the actual lyrics. This is the
    case for example for "The Clash: Police & Thieves" which contains a redirect marker to
    "The Clash: Police and Thieves". If we find this kind of marker, we redo the search with the new
    informations.
    With a special crafted page, it's possible to stuck Sonata in a forever-loop, so we also try not to
    get stuck.

--- sonata/plugins/lyricwiki.py
+++ sonata/plugins/lyricwiki.py
@@ -14,6 +14,7 @@
 ### END PLUGIN INFO
 
 import logging
+import re
 import urllib.request
 
 
@@ -21,16 +22,33 @@ logger = logging.getLogger(__name__)
 BASE_URL = 'http://lyrics.wikia.com/index.php?'
 EMPTY_LYRICS = "&lt;!-- PUT LYRICS HERE (and delete this entire line) -->"
 
+# Redirect marker such as "#REDIRECT [[The Clash:Police And Thieves]]"
+RE_REDIRECT = re.compile(r"#REDIRECT \[\[(.*):(.*)\]\]")
+
 
 def quote(value):
     return urllib.request.quote(str(value).title())
 
 
-def get_lyrics(artist, title):
+def get_lyrics(artist, title, recurse_count=0):
     addr = BASE_URL + 'title=%s:%s&action=edit' % (quote(artist), quote(title))
 
     logger.info("Downloading lyrics from %r", addr)
     content = urllib.request.urlopen(addr).read().decode('utf-8')
+
+    if RE_REDIRECT.search(content):
+        if recurse_count >= 10:
+            # OK, we looped a bit too much, just suppose we couldn't find any
+            # lyrics
+            logger.info("Too many redirects to find lyrics for %r: %r",
+                        artist, title)
+            return None
+
+        new_artist, new_title = RE_REDIRECT.search(content).groups()
+        logger.debug("Lyrics for '%s: %s' redirects to '%s: %s'",
+                     artist, title, new_artist, new_title)
+        return get_lyrics(new_artist, new_title, recurse_count + 1)
+
     lyrics = content.split("&lt;lyrics>")[1].split("&lt;/lyrics>")[0].strip()
 
     if lyrics != "" and lyrics != EMPTY_LYRICS:
