multani/sonata

commit 332b1745e7c6ad1f0871f5bb8a5246badf2f74f3

Migrate local art chooser to GtkBuilder

sonata/main.py
@@ -66,6 +66,7 @@ class Base(object):
 
         self.allow_art_search = None
         self.choose_dialog = None
+        self.image_local_dialog = None
         self.chooseimage_visible = None
 
         self.imagelist = None
@@ -2309,30 +2310,27 @@ class Base(object):
         del pixbuf
         self.call_gc_collect = True
 
-    def image_local(self, _widget):
-        dialog = Gtk.FileChooserDialog(
-            title=_("Open Image"),
-            action=Gtk.FileChooserAction.OPEN,
-            buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-                 Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+    def _image_local_init(self):
+        self.image_local_dialog = self.builder.get_object(
+            'local_artwork_dialog')
         filefilter = Gtk.FileFilter()
         filefilter.set_name(_("Images"))
         filefilter.add_pixbuf_formats()
-        dialog.add_filter(filefilter)
+        self.image_local_dialog.add_filter(filefilter)
         filefilter = Gtk.FileFilter()
         filefilter.set_name(_("All files"))
         filefilter.add_pattern("*")
-        dialog.add_filter(filefilter)
-        preview = ui.image()
-        dialog.set_preview_widget(preview)
-        dialog.set_use_preview_label(False)
-        dialog.connect("update-preview", self.update_preview, preview)
+        self.image_local_dialog.add_filter(filefilter)
+        preview = self.builder.get_object('local_art_preview_image')
+        self.image_local_dialog.connect("update-preview",
+                                        self.update_preview, preview)
+
+    def image_local(self, _widget):
+        if not self.image_local_dialog:
+            self._image_local_init()
         stream = mpdh.get(self.songinfo, 'name', None)
         album = mpdh.get(self.songinfo, 'album', "").replace("/", "")
         artist = self.album_current_artist[1].replace("/", "")
-        dialog.connect("response", self.image_local_response, artist,
-                       album, stream)
-        dialog.set_default_response(Gtk.ResponseType.OK)
         songdir = os.path.dirname(mpdh.get(self.songinfo, 'file'))
         currdir = misc.file_from_utf8(
             os.path.join(self.config.musicdir[self.config.profile_num],
@@ -2345,11 +2343,11 @@ class Base(object):
                 stream)
         else:
             self.local_dest_filename = self.target_image_filename()
-        dialog.show()
+        self.image_local_dialog.show_all()
+        response = self.image_local_dialog.run()
 
-    def image_local_response(self, dialog, response, _artist, _album, _stream):
         if response == Gtk.ResponseType.OK:
-            filename = dialog.get_filenames()[0]
+            filename = self.image_local_dialog.get_filenames()[0]
             # Copy file to covers dir:
             if self.local_dest_filename != filename:
                 shutil.copyfile(filename, self.local_dest_filename)
@@ -2357,7 +2355,7 @@ class Base(object):
             self.artwork.artwork_update(True)
             # Force a resize of the info labels, if needed:
             GObject.idle_add(self.on_notebook_resize, self.notebook, None)
-        dialog.destroy()
+        self.image_local_dialog.hide()
 
     def imagelist_append(self, elem):
         self.imagelist.append(elem)

sonata/ui/sonata.ui
@@ -774,4 +774,83 @@
       <action-widget response="-3">lyrics_search_find_button</action-widget>
     </action-widgets>
   </object>
+  <object class="GtkImage" id="local_art_preview_image">
+    <property name="visible">True</property>
+    <property name="can_focus">False</property>
+  </object>
+  <object class="GtkFileChooserDialog" id="local_artwork_dialog">
+    <property name="can_focus">False</property>
+    <property name="default_width">600</property>
+    <property name="default_height">400</property>
+    <property name="border_width">5</property>
+    <property name="title" translatable="yes">Open Image</property>
+    <property name="role">local_art_dialog</property>
+    <property name="resizable">True</property>
+    <property name="modal">True</property>
+    <property name="window_position">center-on-parent</property>
+    <property name="destroy_with_parent">True</property>
+    <property name="type_hint">dialog</property>
+    <property name="transient_for">main_window</property>
+    <property name="preview_widget">local_art_preview_image</property>
+    <property name="use_preview_label">False</property>
+    <child internal-child="vbox">
+      <object class="GtkBox" id="local_artwork_v_box">
+        <property name="can_focus">False</property>
+        <property name="orientation">vertical</property>
+        <property name="spacing">2</property>
+        <child internal-child="action_area">
+          <object class="GtkButtonBox" id="local_artwork_buttonbox">
+            <property name="can_focus">False</property>
+            <property name="layout_style">end</property>
+            <child>
+              <object class="GtkButton" id="local_art_cancel_button">
+                <property name="label">gtk-cancel</property>
+                <property name="use_action_appearance">False</property>
+                <property name="visible">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_action_appearance">False</property>
+                <property name="use_stock">True</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkButton" id="local_art_open_button">
+                <property name="label">gtk-open</property>
+                <property name="use_action_appearance">False</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="has_default">True</property>
+                <property name="can_default">True</property>
+                <property name="receives_default">True</property>
+                <property name="use_action_appearance">False</property>
+                <property name="use_stock">True</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="pack_type">end</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <placeholder/>
+        </child>
+      </object>
+    </child>
+    <action-widgets>
+      <action-widget response="-6">local_art_cancel_button</action-widget>
+      <action-widget response="-5">local_art_open_button</action-widget>
+    </action-widgets>
+  </object>
 </interface>

commit a630fe99e0e333ba1ce3c0864951abb2977f6e26

artwork: fix broken local artwork chooser dialog
Broken since 332b1745e7c6ad1f0871f5bb8a5246badf2f74f3

sonata/main.py
@@ -66,7 +66,6 @@ class Base:
 
         self.allow_art_search = None
         self.choose_dialog = None
-        self.image_local_dialog = None
         self.chooseimage_visible = None
 
         self.imagelist = None
@@ -2199,24 +2198,22 @@ class Base:
         del pixbuf
         self.call_gc_collect = True
 
-    def _image_local_init(self):
-        self.image_local_dialog = self.builder.get_object(
-            'local_artwork_dialog')
+    def image_local(self, _widget):
+        dialog = self.builder.get_object('local_artwork_dialog')
+
         filefilter = Gtk.FileFilter()
         filefilter.set_name(_("Images"))
         filefilter.add_pixbuf_formats()
-        self.image_local_dialog.add_filter(filefilter)
+        dialog.add_filter(filefilter)
+
         filefilter = Gtk.FileFilter()
         filefilter.set_name(_("All files"))
         filefilter.add_pattern("*")
-        self.image_local_dialog.add_filter(filefilter)
-        preview = self.builder.get_object('local_art_preview_image')
-        self.image_local_dialog.connect("update-preview",
-                                        self.update_preview, preview)
+        dialog.add_filter(filefilter)
 
-    def image_local(self, _widget):
-        if not self.image_local_dialog:
-            self._image_local_init()
+        preview = self.builder.get_object('local_art_preview_image')
+        dialog.connect("update-preview", self.update_preview, preview)
+
         stream = self.songinfo.name
         album = (self.songinfo.album or "").replace("/", "")
         artist = self.album_current_artist[1].replace("/", "")
@@ -2231,11 +2228,11 @@ class Base:
                 stream)
         else:
             self.local_dest_filename = self.target_image_filename()
-        self.image_local_dialog.show_all()
-        response = self.image_local_dialog.run()
+        dialog.show_all()
+        response = dialog.run()
 
         if response == Gtk.ResponseType.OK:
-            filename = self.image_local_dialog.get_filenames()[0]
+            filename = dialog.get_filenames()[0]
             # Copy file to covers dir:
             if self.local_dest_filename != filename:
                 shutil.copyfile(filename, self.local_dest_filename)
@@ -2243,7 +2240,7 @@ class Base:
             self.artwork.artwork_update(True)
             # Force a resize of the info labels, if needed:
             GLib.idle_add(self.on_notebook_resize, self.notebook, None)
-        self.image_local_dialog.hide()
+        dialog.hide()
 
     def imagelist_append(self, elem):
         self.imagelist.append(elem)