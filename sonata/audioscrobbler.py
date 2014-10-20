c2012aa20f9fa74a4a4d93698e59bb607b56528a
Jonathan Ballet <jon@multani.info>
2014-10-20 19:29:31 +0200

    audioscrobbler: add license
    This has been added upstream in commit fda059a634398b6659792aa6e4b36d9f0f4ff9a8
    and I got the authorization by the author to add it back here.
    
    See https://github.com/offmessage/pyscrobbler/commit/fda059a634398b6659792aa6e4b36d9f0f4ff9a8

--- sonata/audioscrobbler.py
+++ sonata/audioscrobbler.py
@@ -264,6 +264,23 @@ Example query script
 Document use of the factory class
     AudioScrobbler
 
+
+License
+=======
+Copyright 2011 Andy Theyers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 """
 
 __author__ = "Andy Theyers <andy@isotoma.com>"
