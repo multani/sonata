91cdb88fa928717b3f1e9f3138fe3b7adfcfc7ff
weyfonk <corentin.neau@gmail.com>
2016-06-23 18:10:27 +0100

    Fix issue related to #73: text update in fullscreen mode
    In addition to what had been noticed about covers not being
    properly updated in fullscreen mode, it seems that the same was
    going for text. Here is an attempt at fixing this.

--- sonata/main.py
+++ sonata/main.py
@@ -699,6 +699,7 @@ class Base:
         self.randommenu.connect('toggled', self.on_random_clicked)
         self.repeatmenu.connect('toggled', self.on_repeat_clicked)
         self.cursonglabel1.connect('notify::label', self.on_currsong_notify)
+        self.cursonglabel1.connect('notify::label', self.fullscreen.on_text_changed)
         self.progressbar.connect('notify::fraction',
                                  self.on_progressbar_notify_fraction)
         self.progressbar.connect('notify::text',
@@ -3148,6 +3149,9 @@ class FullscreenApp:
     def on_artwork_changed(self, artwork_obj, pixbuf):
         self.currentpb = pixbuf
         self.set_image()
+
+    def on_text_changed(self, line1, line2):
+        self.set_text()
 
     def reset(self, *args, **kwargs):
         self.image.set_from_icon_set(ui.icon('sonata-cd'), -1)
