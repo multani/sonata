f4f6f4b124222a0d71e1a3714da6dd18a8ac7b37
Jonathan Ballet <jballet@edgelab.ch>
2019-08-02 13:13:34 +0200

    Replace tabs by spaces

--- sonata/main.py
+++ sonata/main.py
@@ -2515,15 +2515,15 @@ class Base:
             self.iterate_now()
 
     def mpd_prev(self, _widget, _key=None):
-	if self.conn:
+        if self.conn:
             if self.status_is_play_or_pause():
-		# Try to rewind the song if we are advanced enough...
+                # Try to rewind the song if we are advanced enough...
                 at, length = [int(c) for c in self.status['time'].split(':')]
                 if at >= consts.PREV_TRACK_RESTART:
                     self.seek(int(self.status['song']), 0)
-		    return
+                    return
 
-	    # otherwise, just go to the previous song
+            # otherwise, just go to the previous song
             self.mpd.previous()
             self.iterate_now()
 
