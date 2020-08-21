34c00d3b830904b5ad3ef0f131ea9c0a17ba7e34
Jonathan Ballet <jon@multani.info>
2020-08-21 07:36:46 +0200

    Improve downloading lyrics from LyricsWiki
    "edit" action is not limited to logged-in users. It's still possible to
    parse the original content without edit permissions though.
    
    The HTML element holding the lyrics also changed, and is now HTML-encoded.

--- sonata/plugins/lyricwiki.py
+++ sonata/plugins/lyricwiki.py
@@ -13,24 +13,34 @@
 # lyrics_fetching: get_lyrics
 ### END PLUGIN INFO
 
+import html
 import logging
 import re
 from urllib.request import quote, urlopen
+from urllib.error import HTTPError
 
 
 logger = logging.getLogger(__name__)
 BASE_URL = 'http://lyrics.wikia.com/index.php?'
 EMPTY_LYRICS = "&lt;!-- PUT LYRICS HERE (and delete this entire line) -->"
 
+START_MARK = "<div class='lyricbox'>"
+END_MARK = "<div class='lyricsbreak'>"
+
+
 # Redirect marker such as "#REDIRECT [[The Clash:Police And Thieves]]"
 RE_REDIRECT = re.compile(r"#REDIRECT \[\[(.*):(.*)\]\]")
 
 
 def get_lyrics(artist, title, recurse_count=0):
-    addr = BASE_URL + 'title=%s:%s&action=edit' % (quote(artist), quote(title))
+    url = BASE_URL + 'title=%s:%s' % (quote(artist), quote(title))
 
-    logger.info("Downloading lyrics from %r", addr)
-    content = urlopen(addr).read().decode('utf-8')
+    logger.info("Downloading lyrics from %r", url)
+    try:
+        content = urlopen(url).read().decode('utf-8')
+    except HTTPError as exc:
+        logger.warning("Unable to download lyrics from %r: %s", url, exc)
+        return None
 
     if RE_REDIRECT.search(content):
         if recurse_count >= 10:
@@ -45,15 +55,25 @@ def get_lyrics(artist, title, recurse_count=0):
                      artist, title, new_artist, new_title)
         return get_lyrics(new_artist, new_title, recurse_count + 1)
 
-    lyrics = content.split("&lt;lyrics>")[1].split("&lt;/lyrics>")[0].strip()
+    if START_MARK in content and END_MARK in content:
+        lyrics = content.split(START_MARK)[1].split(END_MARK)[0].strip()
+        lyrics = html.unescape(lyrics)
+        lyrics = lyrics.replace("<br />", "\n")
 
-    if lyrics != "" and lyrics != EMPTY_LYRICS:
-        return lyrics
+        if lyrics != "" and lyrics != EMPTY_LYRICS:
+            return lyrics
     else:
-        return None
+        logger.warning("Unable to find LyricsWiki lyrics markers delimitations")
+
+    return None
 
 
 if __name__ == "__main__":
-    import logging
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("artist")
+    parser.add_argument("title")
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
-    print(get_lyrics("Anti-Flag", "Death Of A Nation"))
+    print(get_lyrics(args.artist, args.title))
