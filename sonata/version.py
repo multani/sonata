bf88fdc5d2a066a377710f1a79d36b2b68325435
Jonathan Ballet <jon@multani.info>
2025-04-27 14:20:21 +0200

    Prepare v1.7.1 (#157)

--- sonata/version.py
+++ sonata/version.py
@@ -26,7 +26,7 @@ except ImportError:
     build_ver = None
 
 # Should be the most recent release
-default_version = "v1.7.0"
+default_version = "v1.7.1"
 
 def _version():
     '''Get the version number of the sources
