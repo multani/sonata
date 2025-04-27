943aaaf256c14c1dc2130709588c5d7b62368ec5
Jonathan Ballet <jon@multani.info>
2025-04-27 14:21:40 +0200

    Back to development version

--- sonata/version.py
+++ sonata/version.py
@@ -26,7 +26,7 @@ except ImportError:
     build_ver = None
 
 # Should be the most recent release
-default_version = "v1.7.1"
+default_version = "v1.7.2.dev1"
 
 def _version():
     '''Get the version number of the sources
